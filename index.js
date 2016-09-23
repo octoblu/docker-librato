27060cf728e71813f53b1ae2b5c88b50c546c60d
Peter DeMartini <thepeterdemartini@gmail.com>
2016-09-23 11:34:19 -0700

    v1.1.0 use CLUSTER_NAME AND MACHINE_ID as prefix for source

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const _              = require('lodash')
 const envalid        = require('envalid')
 const librato        = require('librato-node')
 const allcontainers  = require('docker-allcontainers')
 const dockerstats    = require('docker-stats')
 const through        = require('through2')
 const SigtermHandler = require('sigterm-handler')
+const debug          = require('debug')('docker-librato')
 
 const {
   str,
@@ -14,7 +16,8 @@ const {
 const env = envalid.cleanEnv(process.env, {
   LIBRATO_EMAIL: email(),
   LIBRATO_TOKEN: str(),
-  VERBOSE: bool({ default: false }),
+  CLUSTER_NAME: str(),
+  MACHINE_ID: str(),
 });
 
 console.log('Starting Librato...');
@@ -26,8 +29,11 @@ librato.configure({
 
 librato.start();
 
-console.log('Starting Docker event stream...');
+librato.on('error', function(error) {
+  console.error('Librato Error', error)
+})
 
+console.log('Starting Docker event stream...');
 const stats = dockerstats({
   docker: null,
   events: allcontainers({preheat: true, docker:null})
@@ -44,34 +50,40 @@ sigtermHandler.register(function(callback) {
 stats.pipe(through.obj(update));
 
 function update(chunk, enc, callback) {
-  var name = chunk.name;
+  var info
+  const name = chunk.name
+
   try {
-    var info = {
+    info = {
       pcpu: chunk.stats.cpu_stats.cpu_usage.cpu_percent,
       memory: chunk.stats.memory_stats.stats.total_rss,
       memory_usage: chunk.stats.memory_stats.usage,
       memory_limit: chunk.stats.memory_stats.limit,
       memory_cached: chunk.stats.memory_stats.stats.total_cache,
-    };
-
-    if(chunk.stats.network) {
-      info.network_rx = chunk.stats.network.rx_bytes;
-      info.network_tx = chunk.statsr.network.tx_bytes;
     }
+
+    if (chunk.stats.network) {
+      info.network_rx = chunk.stats.network.rx_bytes
+      info.network_tx = chunk.statsr.network.tx_bytes
+    }
+
   } catch(error) {
-    console.error(error)
+    console.error('Docker Usage Error', error)
+    callback(error)
+    return
   }
+
   updateContainer(name, info)
-  callback();
+  callback()
 }
 
-function updateContainer (name, info) {
-  if(env.VERBOSE) {
-    console.log('Updating stats for ' + name, info);
-  }
-  for (var prop in info) {
-    if(info.hasOwnProperty(prop)) {
-      librato.measure('docker-container-' + prop, info[prop], { source: name });
-    }
-  }
+function getSourceName(name) {
+  return `${env.MACHINE_ID}:${env.CLUSTER_NAME}:${name}`;
+}
+
+function updateContainer(name, info) {
+  debug(`Updating stats for ${name}`, info)
+  _.each(info, function(value, key) {
+    librato.measure(`docker-container-${key}`, value, { source: getSourceName(name) })
+  })
 }
